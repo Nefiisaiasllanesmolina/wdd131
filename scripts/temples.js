8564ecc4df969787dbe3cca43e87e83a1a850fac
nefillanes <lla210001@byui.edu>
2024-11-27 17:05:12 -0600

    temples-page

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,19 +1,18 @@
 const currentyear = document.querySelector("#currentyear");
 const lastModified = document.querySelector("#lastModified");
 
-const hamButton = document.querySelector("#menu");
-const navigation = document.querySelector("#navigation");
-
 const today = new Date();
 const year = today.getFullYear();
 
 currentyear.innerHTML = year;
 lastModified.innerHTML = `Last Modification: <span>${document.lastModified}</span>`;
 
-
-
+let hamButton = document.querySelector("#menu");
+let navigation = document.querySelector("#navigation");
+let tittle = document.querySelector("#h1");
 
 hamButton.addEventListener('click', () => {
 	hamButton.classList.toggle("open");
     navigation.classList.toogle("hiddenlist");
+    tittle.classList.toogle("color");
 });
