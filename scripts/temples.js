e9c70dbf5bc89f502631eb4cc9f67176013ca8f7
nefillanes <nefillanes95@gmail.com>
2024-11-27 10:18:38 -0600

    temples-page

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,18 +1,19 @@
 const currentyear = document.querySelector("#currentyear");
 const lastModified = document.querySelector("#lastModified");
 
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector("#navigation");
+
 const today = new Date();
 const year = today.getFullYear();
 
 currentyear.innerHTML = year;
 lastModified.innerHTML = `Last Modification: <span>${document.lastModified}</span>`;
 
-const hamButton = document.querySelector("#menu");
-var navigation = document.querySelector(".navigation");
-var tittle = document.getElementById("#h1");
+
+
 
 hamButton.addEventListener('click', () => {
 	hamButton.classList.toggle("open");
     navigation.classList.toogle("hiddenlist");
-    tittle.classList.toogle("color");
 });
