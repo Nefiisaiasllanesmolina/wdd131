cab761262ffb872256649f809f433ac284290bc5
nefillanes <nefillanes95@gmail.com>
2024-12-05 10:47:23 -0600

    place-page

--- scripts/place.js
+++ scripts/place.js
@@ -13,12 +13,21 @@ lastModified.innerHTML = `Last Modification: <span>${document.lastModified}</spa
 //const windSpeed = document.getElementById("#wind");
 //const temperature = parseInt(document.getElementById("#temperature").value, 10);
 const calculateWindChill = document.querySelector("#windChill");
-const temperature = 10;
-const wind = 5;
+const temperature1 = 10;
+const wind1 = 5;
 
 //nodo = temperature + wind;
-nodo = 13.12 + .6215*(temperature) -11.37*(wind**.16)+.3965*(temperature)*(wind**.16);
-si = nodo.toFixed(2);
-calculateWindChill.innerHTML = `<span>${si}°C</span>`;;
+if (temperature1<=10 && wind1>4.8){
+    function myFunc(temperature, wind) {
+        nodo = 13.12 + .6215*(temperature) -11.37*(wind**.16)+.3965*(temperature)*(wind**.16); calculateWindChill.innerHTML = `<span>${nodo.toFixed(2)}°C</span>`;
+    }
+}
+else{
+    calculateWindChill.innerHTML = "N/A"
+}
+    
+
+myFunc(temperature1, wind1)
+
 
 
