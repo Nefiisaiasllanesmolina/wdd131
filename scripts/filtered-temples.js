9d476f4dd0445e2d1d1600ecde05efef590de235
nefillanes <nefillanes95@gmail.com>
2024-12-09 08:27:47 -0600

    filtered-temples-page

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -100,6 +100,34 @@ const temples = [
     },
   ];
 
+  createTempleCard();
+
+  function createTempleCard() {
+    temples.forEach(temple => {
+      let card = document.createElement("section");
+      let name = document.createElement("h3");
+      let location = document.createElement("p");
+      let dedication = document.createElement("p");
+      let area = document.createElement("p");
+      let img = document.createElement("img");
+
+      name.textContent = temple.templeName;
+      location.innerHTML = `<span class="label">Location:</span> ${temple.location}`;
+      dedication.innerHTML = `<span class="label">Dedicated:</span> ${temple.dedicated}`;
+      area.innerHTML = `<span class="label">Area:</span> ${temple.area} sq ft`;
+      img.setAttribute("src", temple.imageUrl);
+      img.setAttribute("alt", `${temple.templeName} Temple` );
+      img.setAttribute("loading", "lazy");
+
+      card.appendChild(name);
+      card.appendChild(location);
+      card.appendChild(dedication);
+      card.appendChild(area);
+      card.appendChild(img);
+
+      document.querySelector(".container").appendChild(card);
+    });
+  }
   
   var images = temples.map(function(element){
     return '<li>'+element.templeName+' '+element.location+' '+element.dedicated+' '+element.size+element.imageUrl+'</li>'
